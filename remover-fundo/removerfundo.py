2ee39188dd625870231f6dc54e36fded9b4463bc
Antonio Felipe Lopes da Silva <antonio.silva@corp.grdam.com.br>
2024-06-17 15:54:10 -0300

    commit final

---
+++ remover-fundo/removerfundo.py
@@ -0,0 +1,19 @@
+
+# Importing Required Modules 
+from rembg import remove 
+from PIL import Image 
+  
+# Store path of the image in the variable input_path 
+input_path =  './logoname.jpeg' 
+  
+# Store path of the output image in the variable output_path 
+output_path = './logoname.jpeg' 
+  
+# Processing the image 
+input = Image.open(input_path) 
+  
+# Removing the background from the given Image 
+output = remove(input) 
+  
+#Saving the image in the given path 
+output.save(output_path) 
